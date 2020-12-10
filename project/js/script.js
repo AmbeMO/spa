3e1b33f69471ad26c58dc02bc4d23e6c688a95da
Alex <wisdomalex06@gamil.com>
2020-12-11 01:53:37 +0200

    search-page

--- project/js/script.js
+++ project/js/script.js
@@ -3,4 +3,4 @@ $(document).ready(function() {
 		$('.header__burger,.header__menu').toggleClass('active');
 		$('body').toggleClass('lock');// блокірує прокрутку коли ми в медія меню
 	}); 
-});
+})
