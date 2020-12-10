6aa71f9991d43f5f6ffe0d17c08214348894bf51
AmbeMO <milamo@mail.ua>
2020-12-10 22:16:17 +0200

    майже нормальний слайдер

--- project/js/script.js
+++ project/js/script.js
@@ -3,4 +3,29 @@ $(document).ready(function() {
 		$('.header__burger,.header__menu').toggleClass('active');
 		$('body').toggleClass('lock');// блокірує прокрутку коли ми в медія меню
 	}); 
+
+	$('.slider').slick({
+		arrows:false, 
+		dots:true,
+		slidesToShow:6,
+		speed:700,
+		infinite:true,
+		autoplay:true,
+		waitForAnimate:false,
+		centerMode:false,
+		 responsive:[
+{
+    breakpoint: 768,
+    settings: {
+        slidesToShow:2
+    }
+},
+{
+    breakpoint: 550,
+    settings: {
+        slidesToShow:1
+    }
+}
+]
+	});
 });
